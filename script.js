8e9b59058f330ef6b7ec10dd6428add389910fdf
Danar <>
2021-03-31 20:42:28 +0700

    first commit

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
+var toggleWrap = document.getElementById('toggleWrap');
+var toggleBall = document.getElementById('toggleBall');
 var sliderRange = document.getElementById("range");
-var pageviewsNum = document.getElementById("pageviewsValue");
-var priceNum = document.getElementById("pricevalue");
-var chossPlan = document.getElementById("choosePlan");
-var toggle = document.getElementById("chk");
+var priceNum = document.getElementById("priceNum");
+var planPrice = document.getElementById("planPrice");
+var pageviewsNum = document.getElementById("pageviewsNum");
 var discount = 1;
 
-sliderRange.addEventListener("click", changePrice)
-toggle,addEventListener("click", activeToggle)
+
+toggleWrap.addEventListener("click", function() {
+  toggleBall.classList.toggle("transition")
+})
 
 sliderRange.addEventListener("mousemove", function() {
   var x = sliderRange.value;
-  var color = 'linear-gradient(90deg, hsl(174, 77%, 80%)' + x + '%, hsl(0, 0%, 100%)' + x + '%)';
-  sliderRange.style.background = color;
+  var color = "linear-gradient(90deg, hsl(174, 77%, 80%)" + x + "%" + ", hsl(224, 65%, 95%)" + x + "%)"
+  sliderRange.style.background = color
 })
 
+sliderRange.addEventListener("mousemove", changePrice);
+
+toggleWrap.addEventListener("click", activeToggle);
 
 function changePrice(){
   if(sliderRange.value == 0){
-    pageviewsNum.innerHTML = "10K"
-    priceNum.innerHTML = "$" + discount * 8 + ".00"
-   }
-
-  if(sliderRange.value == 25){
-    pageviewsNum.innerHTML = "50K"
-    priceNum.innerHTML = "$" + discount * 12 + ".00"
+    pageviewsNum.innerHTML = "10K";
+    priceNum.innerHTML = "$" + discount * 8 + ".00" ;
   }
-
-  if(sliderRange.value == 50){
-    pageviewsNum.innerHTML = "100K"
-    priceNum.innerHTML = "$" + discount * 16 + ".00"
+  else if(sliderRange.value == 25){
+    pageviewsNum.innerHTML = "50K";
+    priceNum.innerHTML = "$" + discount * 12 + ".00" ;
   }
-
-  if(sliderRange.value == 75){
-    pageviewsNum.innerHTML = "500K"
-    priceNum.innerHTML = "$" + discount * 24 + ".00"
+  else if(sliderRange.value == 50){
+    pageviewsNum.innerHTML = "100K";
+    priceNum.innerHTML = "$" + discount * 16 + ".00" ;
   }
-
-  if(sliderRange.value == 100){
-    pageviewsNum.innerHTML = "1M"
-    priceNum.innerHTML = "$" + discount * 36 + ".00"
+  else if(sliderRange.value == 75){
+    pageviewsNum.innerHTML = "500K";
+    priceNum.innerHTML = "$" + discount * 24 + ".00" ;
+  }else{
+    pageviewsNum.innerHTML = "1M";
+    priceNum.innerHTML = "$" + discount * 36 + ".00" ;
   }
 }
 
 function activeToggle(){
-  if(toggle.checked){
-    chossPlan.innerHTML = '/ year';
+  if(toggleBall.classList.contains("transition")){
     discount = 0.25
+    planPrice.innerHTML = "/year"
     changePrice()
   }else{
-    chossPlan.innerHTML = '/ month'
+    planPrice.innerHTML = "/month"
     discount = 1
     changePrice()
   }
